3fda72d12a34f53f2c26ae6314548df6cc33b72d
Nesly Julio <julionesly@gmail.com>
2022-10-25 11:55:47 -0400

    GenerateQr

--- app/containers/GenerateQr/index.js
+++ app/containers/GenerateQr/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Card, Image } from 'semantic-ui-react';
 
 export default function GenerateQr() {
-  return <div>GenerateQr</div>;
+  const [Response, setResponse] = useState({
+    secret: '',
+    url: '',
+    response: '',
+  });
+  const generate = () => {
+    fetch('https://dev-gecko.ganaloterias.com/authenticator/generarqr')
+      .then(result => result.json())
+      .then(data => setResponse(data));
+  };
+  return (
+    <Card>
+      <Image src={Response.url} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>King-auth</Card.Header>
+        <Card.Description>{Response.secret}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button onClick={generate}>Generar QR</Button>
+      </Card.Content>
+    </Card>
+  );
 }
