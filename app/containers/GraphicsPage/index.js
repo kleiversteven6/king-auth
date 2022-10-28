17ce893a1f18c0962bff49ffd29546f5b29aed58
s5689 <lemirl0ug@gmail.com>
2022-10-28 11:09:55 -0400

    Adicion de Graficos y Mapamundi: Tambien una prueba del api de deteccion de ip del usuario.

--- app/containers/GraphicsPage/index.js
+++ app/containers/GraphicsPage/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import ShareComponent from '../../components/Share';
 
+import Mapamundi from '../../components/mapamundi';
+import GraphicLine from '../../components/GraphicLine';
+
 export default function GraphicsPage() {
   const short = 'google.com';
   const stats = 'gooooooooooogle.com';
+
   return (
     <div>
       Graphics Page
       <Segment>
         <ShareComponent short={short} stats={stats} />
+      </Segment>
+      {/*----------------*/}
+      <Segment>
+        <GraphicLine />
+      </Segment>
+      <Segment>
+        <Mapamundi />
       </Segment>
     </div>
   );
