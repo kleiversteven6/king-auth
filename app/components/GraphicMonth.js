b46cb52f418f484dd57f13e43abecad6025e5125
s5689 <lemirl0ug@gmail.com>
2022-11-05 10:50:58 -0400

    Graficas Lineales por Mes
    Incluidas graficas lineales mensuales al modulo de graficas en la vista Short Url

--- app/components/GraphicMonth.js
+++ app/components/GraphicMonth.js
@@ -2,12 +2,11 @@
 import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import Drilldown from 'highcharts-drilldown';
 
-export default function GraphicLine({ data, drill, short }) {
+export default function GraphicLine({ data, short }) {
   const options = {
     chart: {
-      type: 'column',
+      type: 'spline',
     },
 
     title: {
@@ -15,7 +14,20 @@ export default function GraphicLine({ data, drill, short }) {
     },
 
     xAxis: {
-      type: 'category',
+      categories: [
+        'Ene',
+        'Feb',
+        'Mar',
+        'Abr',
+        'May',
+        'Jun',
+        'Jul',
+        'Ago',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dic',
+      ],
     },
 
     yAxis: {
@@ -36,27 +48,16 @@ export default function GraphicLine({ data, drill, short }) {
 
     tooltip: {
       headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat:
-        '<i><span style="color:{point.color}">{point.name}</span></i>: <b>{point.y}</b> en total<br/>',
+      pointFormat: '<b>{point.y}</b> en total<br/>',
     },
 
     series: [
       {
         name: `Clicks en ${short}`,
-        colorByPoint: true,
         data,
       },
     ],
-    drilldown: {
-      breadcrumbs: {
-        position: {
-          align: 'right',
-        },
-      },
-      series: drill,
-    },
   };
 
-  if (!Highcharts.Chart.prototype.addSeriesAsDrilldown) Drilldown(Highcharts);
   return <HighchartsReact highcharts={Highcharts} options={options} />;
 }
--- app/containers/GraphicsPage/index.js
+++ app/containers/GraphicsPage/index.js
@@ -6,44 +6,50 @@ import { Header, Segment, Button, Tab } from 'semantic-ui-react';
 import ShareComponent from '../../components/Share';
 import SocialComponent from '../../components/Social';
 import GraphicLine from '../../components/GraphicLine';
+import GraphicMonth from '../../components/GraphicMonth';
 import Mapamundi from '../../components/mapamundi';
-import { getWebsite, getGroupCountry } from '../../firebase/api';
-import { getNavigator } from '../../services/services';
+import {
+  getWebsite,
+  getGroupCountry,
+  getGraphLineInfo,
+} from '../../firebase/api';
+// import { getNavigator } from '../../services/services';
 
 export default function GraphicsPage({ match }) {
   const [data, setData] = useState({});
   const [database, setDatabase] = useState({});
   const [detebase, setDetebase] = useState({});
   const [ditibase, setDitibase] = useState({});
+  const [dotobase, setDotobase] = useState({});
 
   const getLink = async () => {
     const querySnapshot = await getWebsite(match.params.id);
 
     const dete = querySnapshot.data();
     setData(dete);
+
+    console.log(data);
   };
 
   const test = async () => {
-    console.log(match.params.id);
     const querySnapshot = await getGroupCountry(match.params.id);
 
     setDatabase(querySnapshot.mapamundi);
-    setDetebase(querySnapshot.line);
+    setDetebase(querySnapshot.weeks);
     setDitibase(querySnapshot.drill);
+    setDotobase(await getGraphLineInfo(match.params.id));
   };
 
   const url = 'https://google.com';
-
   const short = 'google.com';
   const stats = 'gooooooooooogle.com';
 
   const panes = [
     {
-
       menuItem: { content: 'Estadísticas', icon: 'chart area' },
       render: () => (
         <Tab.Pane key={1}>
-          <GraphicLine data={detebase} drill={ditibase} />
+          <GraphicLine data={detebase} drill={ditibase} short={short} />
         </Tab.Pane>
       ),
     },
@@ -57,7 +63,11 @@ export default function GraphicsPage({ match }) {
     },
     {
       menuItem: { content: 'Estadística Mensual', icon: 'chart line' },
-      render: () => <Tab.Pane>Tab 3 Content</Tab.Pane>,
+      render: () => (
+        <Tab.Pane>
+          <GraphicMonth data={dotobase} short={short} />
+        </Tab.Pane>
+      ),
     },
     {
       menuItem: { content: 'Compartir', icon: 'share alternate' },
