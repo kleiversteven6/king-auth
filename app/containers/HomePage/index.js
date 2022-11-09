7bc450d46123952a8ceae527496f48bc90d1e5a8
s5689 <lemirl0ug@gmail.com>
2022-11-09 16:22:43 -0400

    Animacion
    Los dados se lanzan y devuelven el numero que obtuvieron.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,12 +1,38 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- */
+import React, { useEffect, useState } from 'react';
+import { Button, Container } from 'semantic-ui-react';
+import Animacion from '../../components/DiceGame/src';
 
-import React from 'react';
+let Box = {};
 
 export default function HomePage() {
-  return <h1>Inicio!</h1>;
+  const [Dados, setDados] = useState({ d1: '?', d2: '?' });
+
+  useEffect(() => {
+    Box = new Animacion('#dicebox');
+    Box.init();
+  }, []);
+
+  return (
+    <>
+      <h1>aaaaaaaaaaaaaaaaaa</h1>
+      <h3>Dado 1: {Dados.d1}</h3>
+      <h3>Dado 2: {Dados.d2}</h3>
+      <div style={{ textAlign: 'center' }}>
+        <Container
+          id="dicebox"
+          style={{
+            border: '1px solid black',
+            height: '500px',
+            width: '700px',
+          }}
+        />
+        <Button
+          type="button"
+          onClick={() => setDados(Box.roll())}
+          id="rollBtn"
+          content="Tirar de nuevo"
+        />
+      </div>
+    </>
+  );
 }
